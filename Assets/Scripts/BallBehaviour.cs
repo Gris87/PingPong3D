d925c7815b79b998dc764de709466d29e24d4a08
Maxim Shvecov <maxim.shvecov@lge.com>
2014-03-12 16:01:52 +0400

    Textures

--- Assets/Scripts/BallBehaviour.cs
+++ Assets/Scripts/BallBehaviour.cs
@@ -5,11 +5,19 @@ public class BallBehaviour : MonoBehaviour
 {
 	public float acceleration = 0.1f;
 	public float maxSpeed     = 50f;
+	public float boardLimit   = 26f;
+	public int   maxScore     = 3;
+
+	private int playerScore;
+	private int enemyScore;
 
 	// Use this for initialization
 	void Start()
 	{
 		resetPositionAndSpeed(true);
+
+		playerScore = 0;
+		enemyScore  = 0;
 	}
 	
 	// Update is called once per frame
@@ -20,6 +28,36 @@ public class BallBehaviour : MonoBehaviour
 		if (rigidbody.velocity.magnitude>maxSpeed)
 		{
 			rigidbody.velocity=rigidbody.velocity.normalized*maxSpeed;
+		}
+
+		if (transform.position.x>boardLimit)
+		{
+			resetPositionAndSpeed(false);
+
+			playerScore++;
+		}
+		else
+		if (transform.position.x<-boardLimit)
+		{
+			resetPositionAndSpeed(true);
+			
+			enemyScore++;
+		}
+	}
+
+	void OnGui()
+	{
+		if (playerScore==maxScore || enemyScore==maxScore)
+		{
+			if (GUI.Button(new Rect(Screen.width/2-100, Screen.height/2-100, 200, 60), "New game"))
+			{
+				Start();
+			}
+			
+			if (GUI.Button(new Rect(Screen.width/2-100, Screen.height/2+100, 200, 60), "Quit"))
+			{
+				Application.Quit();
+			}
 		}
 	}
 
