3fe4a82a49df86adfb9363a17a23cb6244029ac4
Maxim Shvecov <maxim.shvecov@lge.com>
2014-03-14 14:20:36 +0400

    Handle server creating error

--- Assets/Scripts/Network/NetworkManager.cs
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -4,13 +4,21 @@ using System.Collections;
 
 public class NetworkManager : MonoBehaviour
 {
+	private enum ServerState
+	{
+		ServerInit,
+		ServerStarted,
+		ServerError
+	}
+
 	private const string typeName = "GrisPingPong3D";
 	private const int    port     = 46115;
 
-	private GUIStyle centerTextStyle;
+	private GUIStyle   centerTextStyle;
+	private HostData[] hostList = null;
 
-	private bool isServerMode;
-	private bool serverInitializing;
+	private bool        isServerMode;
+	private ServerState serverState;
 
 	// Use this for initialization
 	void Start()
@@ -36,6 +44,7 @@ public class NetworkManager : MonoBehaviour
 			}
 			else
 			{
+				RefreshHostList();
 			}
 		}
 		else
@@ -46,23 +55,58 @@ public class NetworkManager : MonoBehaviour
 	
 	private void StartServer()
 	{
-		serverInitializing=true;
+		serverState=ServerState.ServerInit;
 
 		string roomName;
-
 		roomName = Environment.UserName;
 
-		Network.InitializeServer(2, port, !Network.HavePublicAddress());
-		MasterServer.RegisterHost(typeName, roomName);
+		if (Network.InitializeServer(2, port, !Network.HavePublicAddress())==NetworkConnectionError.NoError)
+		{
+			MasterServer.RegisterHost(typeName, roomName);
+		}
+		else
+		{
+			serverState=ServerState.ServerError;
+		}
+	}
+
+	private void RefreshHostList()
+	{
+		MasterServer.RequestHostList(typeName);
+	}
+
+	private void JoinServer(HostData hostData)
+	{
+		Network.Connect(hostData);
 	}
 
 	void OnServerInitialized()
 	{
 		Debug.Log("Server initializied");
 
-		serverInitializing=false;
+		serverState=ServerState.ServerStarted;
 	}
-
+	
+	void OnConnectedToServer()
+	{
+		Debug.Log("Joined to server");
+	}
+	
+	void OnMasterServerEvent(MasterServerEvent aEvent)
+	{	
+		switch (aEvent)
+		{
+			case MasterServerEvent.HostListReceived:
+				hostList=MasterServer.PollHostList();
+			break;
+			case MasterServerEvent.RegistrationFailedGameName:
+			case MasterServerEvent.RegistrationFailedGameType:
+			case MasterServerEvent.RegistrationFailedNoServer:
+				serverState=ServerState.ServerError;
+			break;
+		}
+	}
+	
 	void OnGUI()
 	{
 		if (GUI.Button(new Rect(20, 20, 80, 30), "Back"))
@@ -72,14 +116,25 @@ public class NetworkManager : MonoBehaviour
 
 		if (isServerMode)
 		{
-			if (serverInitializing)
+			switch (serverState)
 			{
-				GUI.Label(new Rect(Screen.width/2, Screen.height/2, 1, 1), "Initializing server", centerTextStyle);
+				case ServerState.ServerInit:
+					GUI.Label(new Rect(Screen.width/2, Screen.height/2, 1, 1), "Initializing server", centerTextStyle);
+				break;
+				case ServerState.ServerStarted:
+					GUI.Label(new Rect(Screen.width/2, Screen.height/2, 1, 1), "Waiting for opponent...", centerTextStyle);
+				break;
+				case ServerState.ServerError:
+					GUI.Label(new Rect(Screen.width/2, Screen.height/2, 1, 1), "Impossible to start server", centerTextStyle);
+				break;
+				default:
+					Debug.LogError("Unknown state");
+				break;
 			}
-			else
-			{
-				GUI.Label(new Rect(Screen.width/2, Screen.height/2, 1, 1), "Waiting for opponent", centerTextStyle);
-			}
+		}
+		else
+		{
+
 		}
 	}
 
