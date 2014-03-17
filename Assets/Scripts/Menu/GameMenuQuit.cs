281d6ea82c1a1a4af1150ca39f8e318ad7dd12c6
Maxim Shvecov <maxim.shvecov@lge.com>
2014-03-17 10:33:08 +0400

    Hide quit button in Web

--- Assets/Scripts/Menu/GameMenuQuit.cs
+++ Assets/Scripts/Menu/GameMenuQuit.cs
@@ -3,6 +3,15 @@ using System.Collections;
 
 public class GameMenuQuit : MonoBehaviour
 {
+	// Use this for initialization
+	void Start()
+	{
+		if (Application.platform==RuntimePlatform.OSXWebPlayer || Application.platform==RuntimePlatform.WindowsWebPlayer)
+		{
+			Destroy(gameObject);
+		}
+	}
+
 	void OnMouseUp()
 	{
 		Debug.Log("Application finished");
