5e2071fd9602328daeed56328bd5ab781a71cd36
Maxim Shvecov <maxim.shvecov@lge.com>
2014-03-21 12:36:58 +0400

    Display volume slider and control by mouse

--- Assets/Scripts/Options/BigHorizontalSlider.cs
+++ Assets/Scripts/Options/BigHorizontalSlider.cs
@@ -3,12 +3,20 @@ using System.Collections;
 
 public class BigHorizontalSlider : ModifiableObject
 {
+    private static Texture2D mValueTexture=null;
+
     private float mValue;
     private float mMinimum;
     private float mMaximum;
 
     public BigHorizontalSlider(float value, float minimum, float maximum) : base()
     {
+        if (mValueTexture==null)
+        {
+            mValueTexture=new Texture2D(1, 1);
+            mValueTexture.SetPixel(0, 0, Color.green);
+        }
+
         if (minimum<maximum)
         {
             mMinimum = minimum;
@@ -25,6 +33,59 @@ public class BigHorizontalSlider : ModifiableObject
 
     public void draw(Rect rect)
     {
+        GUI.Box(rect, "");
+
+        Rect rangeRect=new Rect(rect.x+rect.width*0.02f, rect.y+rect.height*0.1f, rect.width*0.96f, rect.height*0.8f);
+        GUI.DrawTexture(new Rect(rangeRect.x, rangeRect.y, rangeRect.width*(mValue-mMinimum)/(mMaximum-mMinimum), rangeRect.height), mValueTexture);
+
+
+
+        ArrayList clickPositions=new ArrayList();
+
+        if (Utils.isTouchDevice)
+        {
+            foreach (Touch touch in Input.touches)
+            {
+                clickPositions.Add(touch.position);
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Vector3 mousePos=Input.mousePosition;
+
+                clickPositions.Add(new Vector2(mousePos.x, Screen.height-mousePos.y));
+            }
+        }
+
+
+
+        if (clickPositions.Count>0)
+        {
+            rect.center      = GUIUtility.GUIToScreenPoint(rect.center);
+            rangeRect.center = GUIUtility.GUIToScreenPoint(rangeRect.center);
+            
+            
+            
+            float curValue=mValue;
+            
+            for (int i=0; i<clickPositions.Count; ++i)
+            {
+                Vector2 clickPos=(Vector2)clickPositions[i];
+                
+                if (rect.Contains(clickPos))
+                {
+                    setValue(mMinimum+((clickPos.x-rangeRect.x)/rangeRect.width)*(mMaximum-mMinimum));
+                    break;
+                }
+            }
+            
+            if (mValue!=curValue)
+            {
+                modificationMade();
+            }
+        }
     }
 
     public void control()
