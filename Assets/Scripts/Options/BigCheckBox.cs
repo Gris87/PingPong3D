f5511087b7255ef7f3714d1977ba2fb06c4b8c94
Maxim Shvecov <maxim.shvecov@lge.com>
2014-03-21 15:36:33 +0400

    BigCheckBox

---
+++ Assets/Scripts/Options/BigCheckBox.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BigCheckBox : ModifiableObject
+{
+    private bool mChecked;
+    
+    public BigCheckBox() : base()
+    {
+        mChecked=false;
+    }
+
+    public void draw(Rect rect)
+    {
+    }
+
+    public void control()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            mChecked=!mChecked;
+            modificationMade();
+        }
+    }
+
+    public bool isChecked()
+    {
+        return mChecked;
+    }
+    
+    public void setChecked(bool value)
+    {        
+        mChecked=value;
+    }
+}
