0b6896f44f80040711a9591592dd032122b742d2
Maxim Shvecov <maxim.shvecov@lge.com>
2014-03-28 11:04:20 +0400

    Update SmartLocalization

--- Assets/SmartLocalization/Scripts/LanguageManager.cs
+++ Assets/SmartLocalization/Scripts/LanguageManager.cs
@@ -1,8 +1,27 @@
 //
 //  LanguageManager.cs
 //
-// Copyright (c) 2013 Niklas Borglund. All rights reserved.
-// @NiklasBorglund
+// The MIT License (MIT)
+//
+// Copyright (c) 2013 Niklas Borglund
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 using UnityEngine;
 using System.Collections;
@@ -517,6 +536,7 @@ public class LanguageManager : MonoBehaviour
         }
 
         string systemLanguage = Application.systemLanguage.ToString();
+#if !UNITY_WP8
         CultureInfo[] cultureInfos = CultureInfo.GetCultures(CultureTypes.AllCultures);
         foreach(CultureInfo info in cultureInfos)
         {
@@ -528,6 +548,10 @@ public class LanguageManager : MonoBehaviour
 
         Debug.LogError("LanguageManager.cs: A system language of this application is could not be found!");
         return "System Language not found!";
+#else
+		Debug.LogError("LanguageManager.cs: GetSystemLanguage() Feature is not currently available in Smart Localization for Windows Phone 8");
+		return defaultLanguage;
+#endif
     }
     /// <summary>
     /// Gets the culture info of the specified string
